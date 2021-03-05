d7b633988769c85d5c16400ade9228c95a27b9df
Umidjon Mukhtarov <ious23@pop-os.localdomain>
2021-03-06 02:28:39 +0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/Mukxtarov/bot-structure
+
+go 1.15
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/magefile/mage v1.11.0 // indirect
+	github.com/sirupsen/logrus v1.8.0 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	golang.org/x/sys v0.0.0-20210305210545-d6cf4ab88724 // indirect
+)
---
+++ pkg/telegram/bot.go
@@ -0,0 +1,76 @@
+package telegram
+
+import (
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/sirupsen/logrus"
+)
+
+// Bot ...
+type Bot struct {
+	BotAPI   *tgbotapi.BotAPI
+	messages chan tgbotapi.Chattable
+}
+
+// NewBot ...
+func NewBot() (*Bot, error) {
+	var err error
+	var bot Bot
+
+	bot.BotAPI, err = tgbotapi.NewBotAPI(os.Getenv("API_TOKEN"))
+	if err != nil {
+		return nil, err
+	}
+
+	bot.BotAPI.Debug = false
+	bot.BotAPI.Buffer = 12 * 15
+	bot.messages = make(chan tgbotapi.Chattable, 300)
+
+	return &bot, nil
+}
+
+func (bot *Bot) StartPolling(stopChan chan struct{}) {
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = 60
+	updateChannel, err := bot.BotAPI.GetUpdatesChan(updateConfig)
+	if err != nil {
+		logrus.Fatalf("StartPolling error receive GetUpdatesChan: %s", err.Error())
+	}
+
+	for update := range updateChannel {
+		select {
+		case <-stopChan:
+			{
+				logrus.Fatalf("Stop Long Pooling !")
+				return
+			}
+		default:
+			{
+				go bot.distributeUpdate(update)
+			}
+		}
+	}
+}
+
+func (bot *Bot) distributeCommand(message *tgbotapi.Message) bool {
+	var isRightCommand = true
+
+	command := message.Command()
+	if command == "" {
+		return false
+	}
+
+	switch command {
+	case "start":
+		{
+			//
+		}
+	default:
+		{
+			return false
+		}
+	}
+
+	return isRightCommand
+}
